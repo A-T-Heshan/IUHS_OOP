0bd2b1b88930595b605f69d199963d41cce45825
WoltreX2002 <134047989+WoltreX2002@users.noreply.github.com>
2023-06-24 21:04:34 +0530

    Update SavingsAccount.java

--- LAB09/SavingsAccount.java
+++ LAB09/SavingsAccount.java
@@ -9,6 +9,7 @@ public class SavingsAccount extends Account{
         balance -= withdrawAmount;
         System.out.println("\nSuccessfully withdrawed Rs." + withdrawAmount);
         System.out.println("New Balance : Rs." + balance );
+        System.out.println("");
     }
     
     
@@ -17,6 +18,7 @@ public class SavingsAccount extends Account{
         super.deposite(depositeAmount);
         System.out.println("\nSuccessfully deposited Rs." + depositeAmount);
         System.out.println("New Balance : Rs." + balance );
+        System.out.println("");
     }
      
     @Override
@@ -24,15 +26,15 @@ public class SavingsAccount extends Account{
         System.out.println("\nAccount No : " + accNumber);
         System.out.println("Name       : " + name);
         System.out.println("Balance    : Rs." + balance);
+        System.out.println("");
     }
 
     @Override
     public void calInterest(double interestRate) {
         interest = balance * interestRate /100/12;
         System.out.println("");
-        System.out.printf("%-10s%12s","Balance ",": Rs." + balance);
-        System.out.println("");
-        System.out.printf("%-10s%12s","Interest ",": Rs." + interest);
+        System.out.println("Balance ",": Rs." + balance);
+        System.out.println("Interest ",": Rs." + interest);
         System.out.println("");
     }
     
