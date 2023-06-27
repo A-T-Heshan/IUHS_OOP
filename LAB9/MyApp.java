2da3da031ec7f9eb050f4ffcec0139ea7f1294ea
WoltreX2002 <134047989+WoltreX2002@users.noreply.github.com>
2023-06-27 10:28:45 +0530

    Add files via upload

---
+++ LAB9/MyApp.java
@@ -0,0 +1,20 @@
+
+
+
+
+public class MyApp {
+    public static void main(String[] args) {
+        SavingsAccount saving = new SavingsAccount(10001, "Amila", 10000);
+        saving.deposite(1000);
+        saving.withdraw(2000);
+        saving.print();
+        saving.calInterest(3);
+        
+        
+        FixedDepositeAccount fixed = new FixedDepositeAccount(20001, "Kamal", 100000,12);
+        fixed.print();
+        fixed.calInterest(20);
+        
+        
+    }
+}
